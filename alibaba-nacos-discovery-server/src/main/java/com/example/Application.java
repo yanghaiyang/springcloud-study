0c32384765bae84f00e24f81ab5e37a98b4ee53a
yanghaiyang <403287415@qq.com>
2020-07-08 20:15:27 +0800

    springcloud-nacos的服务注册的学习

---
+++ alibaba-nacos-discovery-server/src/main/java/com/example/Application.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2020/07/08 0008.
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Slf4j
+    @RestController
+    static class TestController {
+        @GetMapping("/hello")
+        public String hello(@RequestParam String name) {
+            log.info("invoked name = " + name);
+            return "hello " + name;
+        }
+    }
+}
